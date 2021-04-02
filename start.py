df0951f7bccc778127821ffb9ad177f3bfe07cc5
parul mehta <parulm44@gmail.com>
2021-04-03 00:19:43 +0530

    second

--- start.py
+++ start.py
@@ -1,1 +1,21 @@
 print("hello")
+name = "Parul"
+scoree=95
+print("Hello", name , "your score is", scoree)
+print ("hello "+ name + str(scoree))
+print (f"hello {name}")
+
+a=10
+b=20 
+print (a+b)
+
+a="10"
+b="10"
+print(a+b)
+
+a=15
+b=2
+print(f"{a}+{b}={a+b}")
+print("{}+{}={}".format(a,b,a+b))
+print(a/b)
+print(a//b)
