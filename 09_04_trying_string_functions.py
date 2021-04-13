2f8323dfd55b1ff6d7528adc9d0af03cbbc3a786
parul mehta <parulm44@gmail.com>
2021-04-13 20:46:47 +0530

    class 10

---
+++ 09_04_trying_string_functions.py
@@ -0,0 +1,7 @@
+name="Trying oUT diFfereNNT funCtionS"
+print(name.capitalize())
+print(name.replace("diFfereNNT","different"))
+print(name.lower())
+print(name.center(60,'p'))
+print(name.find("oUT"))
+
